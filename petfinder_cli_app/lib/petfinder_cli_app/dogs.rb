141bcd9a4acba8e12c3445e0b722353515313866
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-07-21 23:49:14 +0000

    updated notes get ready to scrape

--- petfinder_cli_app/lib/petfinder_cli_app/dogs.rb
+++ petfinder_cli_app/lib/petfinder_cli_app/dogs.rb
@@ -8,20 +8,11 @@ def self.today
  end
 
 def self.scrape_dogs
-  dog_1 = self.new
-  dog_1.gender = "Male"
-  dog_1.name = "Kevin"
-  dog_1.age = "0-6 months"
-  dog_1.available = "true"
-  dog_1.url = "https://www.petfinder.com/dog/kevin-41781301/nv/las-vegas/pekes-paws-and-tails-nv204/"
+  dogs = []
+  # Go to petfinder, find dogs
+  # Extract the properties
+  # Instantiate availability
 
-  dog_2 = self.new
-  dog_2.gender = "female"
-  dog_2.name = "Nellie"
-  dog_2.age = "0-6 months"
-  dog_2.available = "true"
-  dog_2.url = "https://www.petfinder.com/dog/nellie-42146493/nv/las-vegas/pekes-paws-and-tails-nv204/"
-
-[dog_1, dog_2]
+dogs
   end
 end
