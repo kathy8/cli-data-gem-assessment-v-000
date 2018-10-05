4b783bdf75dcc95ecb46291aa59dd1791e57194b
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-10-05 19:34:20 +0000

    automatically backed up by learn

--- petfinder_cli_app/lib/petfinder_cli_app/dog.rb
+++ petfinder_cli_app/lib/petfinder_cli_app/dog.rb
@@ -2,17 +2,17 @@ class PetfinderCliApp::Dog
 
   attr_accessor :name, :description, :url
 
-  @@all = [] #class variable 
+  @@all = [] #class variable
 
-def self.all #Class reader exposing value on the variable 
-  @@all 
+def self.all #Class reader exposing value on the variable
+  @@all
 end
-  
+
   def initialize (name, url)
     @name = name
     @url = url
   end
-  
+
 def self.reset_all
   @@all.clear
 end
@@ -24,4 +24,4 @@ end
 def self.find_dogs(name) #class Iterate over the all array
   @@all.detect{|dog.name == name}
 end
-end
+
