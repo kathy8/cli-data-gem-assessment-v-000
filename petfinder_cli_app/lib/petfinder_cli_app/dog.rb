488c1d2eca8256547a019b56440d62ee170ab135
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-10-05 19:23:16 +0000

    automatically backed up by learn

--- petfinder_cli_app/lib/petfinder_cli_app/dog.rb
+++ petfinder_cli_app/lib/petfinder_cli_app/dog.rb
@@ -1,8 +1,17 @@
 class PetfinderCliApp::Dog
+
   attr_accessor :name, :description, :url
 
+  @@all = [] #class variable 
+
+def self.all #Class reader exposing value on the variable 
+  @@all 
+end
+  
   def initialize (name, url)
     @name = name
     @url = url
   end
+def self.reset_all
+  @@all.clear
 end
